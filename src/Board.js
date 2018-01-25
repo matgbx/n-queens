9e57e597f2e71f58f4e266b7a30a582f1fba4080
matgbx <matbagnall@gmail.com>
2018-01-24 18:28:38 -0800

    through helper functions

--- src/Board.js
+++ src/Board.js
@@ -79,11 +79,23 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      var counter = 0;
+      var board = this.attributes;
+ 
+      for (var i = 0; i < board.n; i++) {
+        counter += board[rowIndex][i];        
+      }
+      return counter > 1;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
+      var board = this.attributes;
+      for (var i = 0; i < board.n; i++) {
+        if (this.hasRowConflictAt(i)) {
+          return true;      
+        }
+      }
       return false; // fixme
     },
 
@@ -94,11 +106,23 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      var counter = 0;
+      var board = this.attributes;
+      
+      for (var i = 0; i < board.n; i++) {
+        counter += board[i][colIndex];
+      }
+      return counter > 1;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
+      var board = this.attributes;
+      for (var i = 0; i < board.n; i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;      
+        }
+      }
       return false; // fixme
     },
 
@@ -109,12 +133,39 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var rowIndex = 0;
+      var board = this.attributes;
+      var counter = 0;
+      
+      for (var i = majorDiagonalColumnIndexAtFirstRow; i < board.n; i++) {
+        //debugger;
+        
+        if (i < 0) {
+          rowIndex++;
+          
+        } else if (board[rowIndex][i] === 1) {
+          counter++;
+          rowIndex++;
+        } else {
+          rowIndex++;
+        }
+        
+        if (rowIndex >= board.n) {
+          return counter > 1;
+        }
+      }
+      return counter > 1;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var board = this.attributes;
+      for (var i = -(board.n - 1); i < board.n; i++) {
+        if (this.hasMajorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -124,12 +175,39 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var rowIndex = 0;
+      var board = this.attributes;
+      var counter = 0;
+      
+      for (var i = minorDiagonalColumnIndexAtFirstRow; i >= 0; i--) {
+        
+        if (i >= board.n) {
+          rowIndex++;
+          
+        } else if (board[rowIndex][i] === 1) {
+          counter++;
+          rowIndex++;
+        } else {
+          rowIndex++;
+        }
+        
+        if (rowIndex >= board.n) {
+          return counter > 1;
+        }
+      }
+      return counter > 1;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      
+      var board = this.attributes;
+      for (var i = (board.n * 2 - 1); i >= 0; i--) {
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
--- src/solvers.js
+++ src/solvers.js
@@ -16,10 +16,48 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  // make a possible array of all indices using n - for loop
+  // [...Array(n-1).keys()];
+  var possibleI = [...Array(n).keys()];
+  // make empty matrix
+  var board = new Board({n : n});
+  // keep our indice pairs in object
+  var indexObject = {};
+  // function([array of colIndices], rowIndex (n-1))      {
+  
+  var createBoard = function (arrayOfColIndices, rowsRemaining) {
+    // if rows < 0 { 
 
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+    if (rowsRemaining < 0) {
+      //return out of func
+      return;
+    }
+    // for loop (i = 0 ... i < rowIndex){
+    for (var i = 0; i < arrayOfColIndices.length; i++) {
+      //in our object of pairs,
+      //set rowIndex as key, and possible[i] as value
+      indexObject[rowsRemaining] = arrayOfColIndices[i];
+      // function (possible (splice[i]), rows -1);
+      var updatedColIndices = arrayOfColIndices.slice();
+      updatedColIndices.splice(i, 1);
+      createBoard(updatedColIndices, (rowsRemaining - 1));
+    }
+  };
+  
+  // run function on (possiblesArray, n -1);
+  createBoard(possibleI, n - 1);
+  //   update empty matrix with our indice pairs from object
+  for (var key in indexObject) {
+    //debugger;
+    console.log('our board', JSON.stringify(board));
+    var row = key;
+    var col = indexObject[key];
+    board.attributes[row][col] = 1;
+  }
+  // return matrix solution
+  // }
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(board));
+  return indexObject;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
